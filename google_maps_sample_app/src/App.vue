3a5551cd7b3553039511a5d7ec6eea1cd556096b
yoheioda0127 <maikemi00romans@gamil.com>
2021-04-26 07:10:43 +0900

    Update App.vue

--- google_maps_sample_app/src/App.vue
+++ google_maps_sample_app/src/App.vue
@@ -5,6 +5,25 @@
 </template>
 
 <script>
+  import axios from "axios";
 
+  import $Scriptjs from "scriptjs"; 
+  import GoogleMap from "./components/Map.vue"; 
 
+  const API_KEY = "AIzaSyBfhcfqlsIYAkc9Cw77AjwOb7BkOM-f3G0";
+  const API_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBfhcfqlsIYAkc9Cw77AjwOb7BkOM-f3G0&libraries=places";;
+  
+  export default {
+
+  }
+</script>
+
+<style> 
+  html,body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100vh;
+  }
 </style>
