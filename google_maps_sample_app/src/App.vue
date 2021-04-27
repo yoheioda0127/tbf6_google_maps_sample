b4f9ffd15f9b3de4ba41ad22fb8b992aaab4aa8e
yoheioda0127 <maikemi00romans@gamil.com>
2021-04-27 19:05:09 +0900

    Update App.vue

--- google_maps_sample_app/src/App.vue
+++ google_maps_sample_app/src/App.vue
@@ -14,9 +14,67 @@
   const API_KEY = "AIzaSyBfhcfqlsIYAkc9Cw77AjwOb7BkOM-f3G0";
   const API_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBfhcfqlsIYAkc9Cw77AjwOb7BkOM-f3G0&libraries=places";;
   
-  export default {
-
-  }
+  export default { 
+  name: "app", 
+  data() { 
+    return { 
+      initialPlace: { 
+        lat: 35.681236, 
+        lng: 139.767125 
+      }, 
+      zoom: 17, 
+      map: null, 
+      google: null, 
+      result: "" 
+    };
+  }, 
+  created() { 
+    $Scriptjs(API_URL, () => this.initMap()); 
+  }, 
+  methods: { 
+    initMap() { 
+      this.google = google; 
+      this.map = new google.maps.Map(mapElement, { 
+        center: this.initialPlace, 
+        zoom: this.zoom, 
+        disableDefaultUI: true 
+      }); 
+    }, 
+    reverseGeocoding(latlng) { 
+      let _self = this; 
+      axios 
+        .get("https://maps.googleapis.com/maps/api/geocode/json", { 
+          params: { language: "ja", latlng: latlng, key: API_KEY } 
+        }) 
+        .then(response => { 
+          _self.result = response.data.results[0].formatted_address; 
+        }) 
+        .catch(error => { 
+          alert("Error: " + error); 
+        }); 
+    }, 
+    placeAutocomplete(keyword) { 
+      $Scriptjs(API_URL, () => { 
+        let service = new google.maps.places.AutocompleteService(); 
+        let descriptions = []; 
+        service.getQueryPredictions( 
+          { input: keyword }, 
+          (predictions, status) => { 
+            if (status === "OK") { 
+              predictions.forEach(ele => { 
+                descriptions.push(ele.description); 
+              }); 
+            } 
+          } 
+        ); 
+        this.result = descriptions; 
+      }); 
+    } 
+  }, 
+  components: { 
+    GoogleMap
+  } 
+  }; 
 </script>
 
 　
